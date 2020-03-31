daab083770e5fc829976d831baf828fd27190ba3
Sandesh VAKALE <sandyrv.vakale@gmail.com>
2020-03-31 21:29:21 +0200

    changes for coins list

--- App/Containers/CoinsScreen.js
+++ App/Containers/CoinsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
@@ -11,6 +11,9 @@ import colors from '../Themes/Colors'
 import { DateAndTime } from '../Components/DateAndTime'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import _ from 'lodash'
+import { Colors } from '../Themes'
+import SvgUri from 'react-native-svg-uri'
+import { LineChart } from 'react-native-chart-kit'
 
 class CoinsScreen extends Component {
   constructor (props) {
@@ -23,7 +26,7 @@ class CoinsScreen extends Component {
 
     const { stats, coins } = this.props
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: colors.silver }}>
         <Header containerStyle={{ backgroundColor: colors.bloodOrange }}
                 rightComponent={<DateAndTime/>} centerComponent={{
           text: 'Coins',
@@ -34,6 +37,56 @@ class CoinsScreen extends Component {
           size={34}
           color={colors.silver}
           onPress={() => this.setState({ isVisible: true })}/>}/>
+        <FlatList data={coins.payload.data.coins}
+                  showsVerticalScrollIndicator={false}
+                  contentContainerStyle={{borderColor: Colors.coal, backgroundColor: colors.silver }}
+                  renderItem={
+                    (item) => <View style={{ flexDirection: 'row', alignItems: 'center', padding: 5, borderWidth: 1, margin: 5, borderColor: item.item.color, borderRadius: 12}} >
+                      <SvgUri
+                        style={{ paddingRight: 5, flex: 0.2, alignItems: 'center'}}
+                        width={40}
+                        height={40}
+                        source={{ uri: item.item.iconUrl }}
+                      />
+                      <View style={{ flex: 0.2, flexDirection: 'column' }}>
+                        <Text numberOfLines={1} style={{ fontSize: 14, fontWeight: 'bold', color: colors.bloodOrange }} >{item.item.name}</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold', color: item.item.color }} >{item.item.symbol}</Text>
+                      </View>
+                      <View style={{ alignItems: 'center', flex: 0.4 }}>
+                        <LineChart
+                          data={{
+                            // labels: timePeriod === '24h' ? hours : timePeriod === '7d' ? days7 : days30,
+                            datasets: [
+                              {
+                                data: item.item.history,
+                              },
+                            ],
+                          }}
+                          withInnerLines={false}
+                          withDots={false}
+                          withOuterLines={false}
+                          withVerticalLabels={false}
+                          withHorizontalLabels={false}
+                          withShadow={false}
+                          width={150} // from react-native
+                          height={60}
+                          yAxisInterval={1} // optional, defaults to 1
+                          chartConfig={{
+                            backgroundColor: colors.bloodOrange,
+                            backgroundGradientFrom: colors.silver,
+                            backgroundGradientTo: colors.silver,
+                            decimalPlaces: 2, // optional, defaults to 2dp
+                            color: (opacity = 1) => Math.sign(item.item.change) === -1 ? colors.error : colors.lightgreen,
+                          }}
+                          style={{alignItems: 'center', marginLeft: -10}}
+                        />
+                      </View>
+                      <View style={{ flex: 0.2, flexDirection: 'column' }}>
+                        <Text style={{ fontSize: 16, fontWeight: 'bold', color: colors.bloodOrange }} >{coins.payload.data.base.sign} {_.ceil(item.item.price, 2)}</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold', color: Math.sign(item.item.change) === -1 ? colors.error : colors.lightgreen, flex: 0.3 }} >{item.item.change}%</Text>
+                      </View>
+                    </View>
+                  }/>
         <Overlay height={450} width={'95%'} isVisible={this.state.isVisible}
                  onBackdropPress={() => this.setState({ isVisible: false })}>
           <Text h2 h2Style={{
